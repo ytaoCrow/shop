a8354a5b885748032f7333666f966bed9fc5659a
ytaoCrow <kafka880407@gmail.com>
2022-05-02 01:14:24 +0800

    初始化 maven application

---
+++ shop/src/main/java/com/ytaocrow/shop/model/Product.java
@@ -0,0 +1,16 @@
+package com.ytaocrow.shop.model;
+
+import java.util.Date;
+
+public class Product {
+    private  Integer productId;
+    private  String productName;
+    private  String category;
+    private  String imageUrl ;
+    private  Integer price;
+    private  Integer stock;
+    private  String description;
+    private Date createdDate;
+    private Date lastModifiedDate;
+
+}
