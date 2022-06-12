bb6cf9814d239f414e3671886763ff3fa73e1c09
ytaoCrow <kafka880407@gmail.com>
2022-06-12 22:16:10 +0800

    Account - login password ues MD5 hashedPassword

--- shop/src/main/java/com/ytaocrow/shop/service/Impl/UserServiceImpl.java
+++ shop/src/main/java/com/ytaocrow/shop/service/Impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -35,6 +36,11 @@ public class UserServiceImpl implements UserService {
             throw  new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        // MD5 Hash
+
+        String hashedPassword =
+                DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
         //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -43,12 +49,18 @@ public class UserServiceImpl implements UserService {
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        //check user
         if(user == null){
             log.warn("該email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if (user.getPassword().equals((userLoginRequest.getPassword()))) {
+        //MD5 HashedPassword
+        String hashedPassword =
+                DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        //比較密碼
+        if (user.getPassword().equals(hashedPassword)) {
             return  user;
         }else {
 
